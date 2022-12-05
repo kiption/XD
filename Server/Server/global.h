ba93676ff176287fc6c3a1f24cd3e664aad27a1a
Kim Seung Hwan <84659754+sh4816@users.noreply.github.com>
2022-12-05 17:49:52 +0900

    #12-05 김승환 이동 스칼라값 & 회전제한각도 조정
    상하, 전진, 후진 이동 스칼라 값 조정
    회전 제한 각도 조정

--- Server/Server/global.h
+++ Server/Server/global.h
@@ -3,10 +3,10 @@
 #define PI 3.141592654f
 
 #define MOVE_SCALAR 0.5f	// 이동 힘
-#define ENGINE_SCALAR 0.8f	// 헬기 엔진 출력 (상승, 하강 정도조절)
+#define ENGINE_SCALAR 0.9f	// 헬기 엔진 출력 (상승, 하강 정도조절)
 
-#define SENSITIVITY 1.5f	// 마우스 감도
+#define SENSITIVITY 1.1f	// 마우스 감도
 
 #define ROLL_LIMIT 60.f		// 회전 각도제한
-#define PITCH_LIMIT 45.f
+#define PITCH_LIMIT 20.f
 #define YAW_LIMIT 90.f
