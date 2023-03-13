9566ebd81aa1aa93889c63f278d88d8a8185cdee
Kim Seung Hwan <84659754+sh4816@users.noreply.github.com>
2023-03-13 23:57:36 +0900

    누락된 코드 추가 커밋
    .

--- Client/PvE/REVENGER/ObjectsInfo.h
+++ Client/PvE/REVENGER/ObjectsInfo.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <array>
 #include <queue>
-#include "../../../Server/Server/protocol.h"
+#include "../../../Server/RelayServer/Protocol.h"
 
 enum OBJECT_STATE { OBJ_ST_EMPTY, OBJ_ST_STANDBY, OBJ_ST_LOGOUT, OBJ_ST_RUNNING };
 struct ObjectsInfo
