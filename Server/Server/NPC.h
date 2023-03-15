455d6e31dd394aa46d09352d152b5b23ae2af477
taps <seichulee@naver.com>
2023-03-16 04:41:23 +0900

    #03-16 이세철 NPC 세분화 & 기능 설계 중

--- Server/Server/NPC.h
+++ Server/Server/NPC.h
@@ -3,14 +3,14 @@
 #include "Constant.h"
 #include "MyVectors.h"
 
-enum NpcType{NPC_Helicopter, NPC_Bunker, NPC_Terret};
+enum NpcType { NPC_Helicopter, NPC_Bunker, NPC_Terret };
 
-class NPC{
+class NPC {
 private:
 	int m_ID;									// ID는 7001~부터 시작
 	int m_NpcType;
 	float m_pitch, m_yaw, m_roll;					// Rotated Degree
-	
+
 	XMFLOAT3 m_Pos;
 	XMFLOAT3 m_saveOrgPos;
 	Coordinate m_curr_coordinate;				// 현재 Look, Right, Up Vectors
@@ -19,6 +19,8 @@ private:
 	float m_theta;
 	float m_range;					// 임시 변수 재 제작시 사라질 운명
 	float m_Acc;
+
+	float m_Distance;
 
 public:
 	// ===========================================
@@ -43,6 +45,8 @@ public:
 	void SetTheta(float t);
 	void SetRange(float r);
 	void SetAcc(float acc);
+	void SetDistance(float dis);
+	
 	// ===========================================
 	// =============       GET      ==============
 	// ===========================================
@@ -58,6 +62,7 @@ public:
 	float GetTheta();
 	float GetRange();
 	float GetAcc();
+	float GetDistance();
 
 public:
 	// ===========================================
@@ -66,7 +71,42 @@ public:
 	void Move();					// 추후에 A* 알고리즘 추가할 예정 현재는 특정 운동만 수행
 	void MovetoRotate();
 	XMFLOAT3 NPCcalcRotate(XMFLOAT3 vec, float pitch, float yaw, float roll);
+	virtual void Get_Distance_Detection(XMFLOAT3 vec);
+	virtual void FindTarget();
+
+};
+
+class Stage1Enemy : public NPC
+{
+private:
 
 
+public:
+	int m_n1STEnemy = 60;
+
+public:
+	Stage1Enemy();
+	~Stage1Enemy();
+
+	virtual void Get_Distance_Detection(XMFLOAT3 vec);
+	
+	void ChaseToPlayer();
+};
+
+class Stage2Enemy : public NPC
+{
+private:
+
+public:
+	int m_n2STEnemy = 40;
+
+public:
+
+	Stage2Enemy();
+	~Stage2Enemy();
+
+	virtual void Get_Distance_Detection(XMFLOAT3 vec);
+
+	void ChaseToPlayer();
 };
 
