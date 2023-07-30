06f3e1bdd9125891534596a7504cb0ce23e39b7e
kiption <89969740+kiption@users.noreply.github.com>
2023-07-30 19:29:03 +0900

    #클라이언트 최종 릴리즈

--- Client/PvE/REVENGER/GameFramework.cpp
+++ Client/PvE/REVENGER/GameFramework.cpp
@@ -2017,11 +2017,11 @@ void CGameFramework::FrameAdvance()
 
 	if (m_nMode == SCENE1STAGE)
 	{
-		m_GameTimer.GetFrameRate(m_pszFrameRate + 12, 37);
-		size_t nLength = _tcslen(m_pszFrameRate);
-		XMFLOAT3 xmf3Position = ((CHumanPlayer*)((Stage1*)m_pScene)->m_pPlayer)->GetPosition();
-		_stprintf_s(m_pszFrameRate + nLength, 70 - nLength, _T("(%5.1f, %5.1f, %5.1f)"), xmf3Position.x, xmf3Position.y, xmf3Position.z);
-		::SetWindowText(m_hWnd, m_pszFrameRate);
+		//m_GameTimer.GetFrameRate(m_pszFrameRate + 12, 37);
+		//size_t nLength = _tcslen(m_pszFrameRate);
+		//XMFLOAT3 xmf3Position = ((CHumanPlayer*)((Stage1*)m_pScene)->m_pPlayer)->GetPosition();
+		//_stprintf_s(m_pszFrameRate + nLength, 70 - nLength, _T("(%5.1f, %5.1f, %5.1f)"), xmf3Position.x, xmf3Position.y, xmf3Position.z);
+		//::SetWindowText(m_hWnd, m_pszFrameRate);
 	}
 }
 
