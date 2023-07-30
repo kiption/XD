54ac7552d124f7ca2a5ca27e218ef2a0d83034ed
Kim Seung Hwan <84659754+sh4816@users.noreply.github.com>
2023-07-31 06:27:37 +0900

    #07-31 김승환 NPC서버 수정
    로직서버가 다운되었을때 NPC서버가 다른 로직서버로 연결되도록 수정

--- Server/NpcServer/NpcServer/NpcServer.cpp
+++ Server/NpcServer/NpcServer/NpcServer.cpp
@@ -50,6 +50,8 @@ using namespace DirectX::PackedVector;
 using namespace std;
 using namespace chrono;
 
+int a_lgcsvr_num;										// Active상태인 메인서버
+
 //============================================================
 system_clock::time_point last_send_checkpos_time;	// 마지막으로 check_pos패킷 보낸 시간
 mutex time_lock;	// 서버시간 lock
@@ -447,32 +449,99 @@ public:
 
 		int ret = WSARecv(sock, &recv_over.wsabuf, 1, 0, &recv_flag, &recv_over.overlapped, 0);
 		if (ret != 0 && GetLastError() != WSA_IO_PENDING) {
-			cout << "WSARecv Error - " << ret << endl;
-			cout << GetLastError() << endl;
+			//cout << "WSARecv Error - " << ret << endl;
+			//cout << GetLastError() << endl;
 		}
 	}
 
-	void do_send(void* packet)
-	{
-		OVER_EX* s_data = new OVER_EX{ reinterpret_cast<char*>(packet) };
-		ZeroMemory(&s_data->overlapped, sizeof(s_data->overlapped));
-
-		int ret = WSASend(sock, &s_data->wsabuf, 1, 0, 0, &s_data->overlapped, 0);
-		if (ret != 0 && GetLastError() != WSA_IO_PENDING) {
-			cout << "WSASend Error - " << ret << endl;
-			cout << GetLastError() << endl;
-		}
-	}
+	void do_send(void* packet);
 
 	void send_npc_init_packet(int npc_id);
 	void send_npc_move_packet(int npc_id);
 	void send_npc_rotate_packet(int npc_id);
 	void send_npc_attack_packet(int npc_id);
 };
-
 HANDLE h_iocp;											// IOCP 핸들
-int a_lgcsvr_num;										// Active상태인 메인서버
 array<SERVER, MAX_LOGIC_SERVER> g_logicservers;			// 로직서버 정보
+
+void SERVER::do_send(void* packet)
+{
+	OVER_EX* s_data = new OVER_EX{ reinterpret_cast<char*>(packet) };
+	ZeroMemory(&s_data->overlapped, sizeof(s_data->overlapped));
+
+	int ret = WSASend(sock, &s_data->wsabuf, 1, 0, 0, &s_data->overlapped, 0);
+	if (ret != 0 && GetLastError() != WSA_IO_PENDING) {
+		//cout << "WSASend Error - " << ret << endl;
+		//cout << GetLastError() << endl;
+		if (GetLastError() == WSAECONNRESET) {
+			closesocket(g_logicservers[a_lgcsvr_num].sock);
+			cout << "[Failover] Logic Server[" << a_lgcsvr_num << "]의 다운이 감지되었습니다." << endl;	// server message
+
+			// 서버번호를 바꿔가면서 비동기Connect를 재시도합니다.
+			if (a_lgcsvr_num == 0)		a_lgcsvr_num = 1;
+			else if (a_lgcsvr_num == 1)	a_lgcsvr_num = 0;
+			int new_portnum = a_lgcsvr_num + PORTNUM_LGCNPC_0;
+			//cout << "[ConnectEX Failed] ";
+			//cout << "Logic Server[" << a_lgcsvr_num << "] (PORTNUM:" << new_portnum << ")로 다시 연결합니다. \n" << endl;
+
+			SOCKET temp_s = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+			GUID guid = WSAID_CONNECTEX;
+			DWORD bytes = 0;
+			LPFN_CONNECTEX connectExFP;
+			::WSAIoctl(temp_s, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &connectExFP, sizeof(connectExFP), &bytes, nullptr, nullptr);
+			closesocket(temp_s);
+
+			SOCKADDR_IN logic_server_addr;
+			ZeroMemory(&logic_server_addr, sizeof(logic_server_addr));
+			logic_server_addr.sin_family = AF_INET;
+			g_logicservers[a_lgcsvr_num].sock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);     // 실제 연결할 소켓
+			int ret = ::bind(g_logicservers[a_lgcsvr_num].sock, reinterpret_cast<LPSOCKADDR>(&logic_server_addr), sizeof(logic_server_addr));
+			if (ret != 0) {
+				cout << "Bind Error - " << ret << endl;
+				cout << WSAGetLastError() << endl;
+				exit(-1);
+			}
+
+			OVER_EX* con_over = new OVER_EX;
+			con_over->process_type = OP_CONNECT;
+			HANDLE hret = CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_logicservers[a_lgcsvr_num].sock), h_iocp, new_portnum, 0);
+			if (NULL == hret) {
+				cout << "CreateIoCompletoinPort Error - " << ret << endl;
+				cout << WSAGetLastError() << endl;
+				exit(-1);
+			}
+
+			ZeroMemory(&logic_server_addr, sizeof(logic_server_addr));
+			logic_server_addr.sin_family = AF_INET;
+			logic_server_addr.sin_port = htons(new_portnum);
+			// 1. 메인서버와 NPC서버가 루프백에서 동작할 때
+			inet_pton(AF_INET, IPADDR_LOOPBACK, &logic_server_addr.sin_addr);
+
+			// 2. 메인서버와 NPC서버가 다른 PC에서 동작할 떄
+			/*
+			if (a_lgcsvr_num == 0) {
+				inet_pton(AF_INET, IPADDR_LOGIC0, &logic_server_addr.sin_addr);
+			}
+			else if (a_lgcsvr_num == 1) {
+				inet_pton(AF_INET, IPADDR_LOGIC1, &logic_server_addr.sin_addr);
+			}
+			*/
+
+			BOOL bret = connectExFP(g_logicservers[a_lgcsvr_num].sock, reinterpret_cast<sockaddr*>(&logic_server_addr), sizeof(SOCKADDR_IN),
+				nullptr, 0, nullptr, &con_over->overlapped);
+			if (FALSE == bret) {
+				int err_no = GetLastError();
+				if (ERROR_IO_PENDING == err_no) {
+					//cout << "Server Connect 재시도 중...\n" << endl;
+				}
+				else {
+					cout << "ConnectEX Error - " << err_no << endl;
+					cout << WSAGetLastError() << endl;
+				}
+			}
+		}
+	}
+}
 
 void SERVER::send_npc_init_packet(int npc_id)
 {
@@ -2925,6 +2994,7 @@ void disconnect(int target_id, int target)
 	switch (target) {
 	case SESSION_LOGIC:
 	{
+		
 		break;
 	}
 	case SESSION_NPC:
